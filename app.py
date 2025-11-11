daaf3cf9b0e06c1ec5a91d38af6b604fc0867a30
Shehan Tharindu <tharindushehan732@gmail.com>
2025-11-11 12:19:09 +0530

    Add new feature / update app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD pipeline! ✅ Deployed automatically"
-
-@app.route("/fail")
-def fail():
-    return "Hello from CI/CD pipeline! ✅ Deployed fail"
+    return "Hello from Flask CI/CD pipeline on Vercel!"
+    return "Helloooo "
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

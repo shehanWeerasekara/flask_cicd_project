61437c5b7a392477a74eb1bea1d85fd5a70762b6
Shehan Tharindu <tharindushehan732@gmail.com>
2025-11-11 12:37:22 +0530

    test ci/cd workflow

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD pipeline on Vercel! hey how are you"
+    return "hello world"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

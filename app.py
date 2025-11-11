8d6a27fd6df8684818e8e73e68fb226d49ca9c22
Shehan Tharindu <tharindushehan732@gmail.com>
2025-11-11 13:00:22 +0530

    new changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+    return "hello world.... how is day today"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
